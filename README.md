6c7681c059a5a65e8c30fdf0d703f8a9836b174d
Chris Tohill <chris@visualoverdose.ca>
2017-04-24 21:49:09 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# Facebook Reaction Counter
+
+`Note: This script SHOULD NOT be used on a public/production environment as it exposes your Facebook API secret. Working around this should be on the TODO list!`
+
+#### What does this thing do?
+
+A jQuery plugin for retrieving reaction totals from a Facebook post using the Facebook API.  
+
+Initially developed to do live polls streaming to Facebook Live, but the script itself could probably be used for other things.
+
+If you're new to HTML/CSS/JavaScript, see the examples folder for some basic examples using the script.
+
+#### License
+Released under the MIT license - http://opensource.org/licenses/MIT
+
+## Requirements
+* jQuery v1.x +
+* Web server (makes asynchronous calls)
+* An app created at developers.facebook.com (including App ID, and App Secret)
+
+## Installation
+
+The files are available either by cloning/downloading this repo  
+
+`git clone christohill/fb-reaction-counter`  
+
+or via Bower  
+  
+`bower install christohill/fb-reaction-counter`
+
+## Usage
+
+#### Link required files
+Require jQuery (if not already included) and include the Reaction Counter script  
+
+*Change these paths to fit your project*
+```javascript
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="../jquery.fb-reaction-counter.js"></script>
+```
+
+#### Basic HTML Markup
+Use the `data-reaction` attribute to choose which reactions you'd like to display. A child element will be added for the counter `fbr-counter` and the current count will be inserted there.  
+  
+Available reactions are: `love, like, haha, wow, angry, sad`  
+```html
+<main class="reaction-container">
+  <section data-reaction="love"></section>
+  <section data-reaction="like"></section>
+  <section data-reaction="haha"></section>
+  <section data-reaction="wow"></section>
+  <section data-reaction="angry"></section>
+  <section data-reaction="sad"></section>
+</main>
+```
+##### Example output
+```html
+<section data-reaction="love">
+  <span class="fbr-counter">1987</span>
+</section>
+```
+
+### Initialize plugin
+
+```javascript
+<script>
+  $(document).ready(function() { // When the document is ready
+
+  	// Initialize the script on the .reaction-container element
+    $('.reaction-container').reactionCounter({
+
+      clientID: "203948720389472",
+      clientSecret: "s98d7f6s987df687787df87f68",
+      pageURL: 'https://facebook.com/marketing',
+      postID: "10155446865956337",
+      refreshTime: 5,
+      appendPrepend: 'prepend',
+      totalType: 'selected'
+      totalSelector: '.fbr-total'
+
+    });
+
+  });
+</script>
+```
+
+#### Plugin options
+|Option | Description | Options | Default |
+|-------|------------|---------|---------|
+|clientID | The client ID supplied from the app you created on developers.facebook.com | *N/A* | NULL (required)
+|clientSecret | The client secret supplied from the app you created on developers.facebook.com | *N/A* | NULL (required)
+|pageURL | Page URL of your Facebook page (Note: This can't be used for standard user pages) | *N/A* | NULL (required)
+|postID | The ID of the post you would like to get the in reaction counts from | *N/A* | NULL (required)
+|refreshTime | How often to poll Facebook and update the data (measured in seconds) | `integer` | 5
+|appendPrepend | Choose to add the counter before or after the content in the element | `append` or `prepend` | append
+|defaultCount | Set a starting number for the counters | `integer` | 0
+|totalType | Choose how to calculate the total amount of votes. Either from the reactions you've selected, or all 6 reactions | `all` or `selected` | selected
+|totalSelector | An HTML element to display the total amount of reactions based on `totalType` | `element` | `.fbr-total`
---
+++ jquery.fb-reaction-counter.js
@@ -0,0 +1,246 @@
+(function($) {
+
+	$.fn.reactionCounter = function(options) {
+
+		// Plugin settings
+		var settings = $.extend({
+			pageURL: '',
+			clientID: '',
+			secretID: '',
+			postID: '',
+			refreshTime: 5,
+			defaultCount: 0,
+			showOutput: true,
+			totalSelector: '.fbr-total',
+			totalType: 'selected'
+		}, options);
+
+		var pageID;
+		var reactions = [];
+		var container = this;
+		var sections = this.find('[data-reaction]').not('p');
+		var accessToken = getAccessToken(settings.clientID, settings.clientSecret);
+
+		// Make sure pageID is set properly
+		if(pageID = getPageID(settings.pageURL, accessToken)) {
+			settings.postID = getPageID(settings.pageURL, accessToken) + "_" + settings.postID;
+		}
+
+		// If we have an access token and a proper postID, start the script
+		if(accessToken && settings.postID != null) {
+
+			// Init the script
+			init();
+
+		}
+
+
+
+
+
+
+		/**
+		 * Start the script and poll the services
+		 * @return NULL
+		 */
+		function init() {
+
+			// Loop through all of the reaction sections
+			$.each(sections, function(index, value) {
+
+				// Add sections reaction attribute to reactions array
+				reactions.push($(this).data('reaction'));
+
+				// Add the counter element to the section
+				var counter = $('<span>', {'class': 'fbr-counter'});
+
+				if(settings.appendPrepend === 'append') {
+					$(this).append(counter);
+				}else {
+					$(this).prepend(counter);
+				}
+
+			});
+
+			setInterval(getTotals, parseInt(settings.refreshTime) * 1000);
+	    	getTotals();
+
+	    }
+
+
+
+
+
+	    /**
+	     * Grab the Facebook page ID from a URL
+	     * @param {String} url			The URL of the Facebook page
+	     * @param {String} accessToken	App access token
+	     * @return {Int} 				The integer value of the ID
+	     */
+	    function getPageID(url, accessToken) {
+
+	    	var segment = url.substr(url.lastIndexOf('/') + 1);
+
+	    	$.ajax({
+
+	    		url: 'https://graph.facebook.com/v2.9/' + segment + '?access_token=' + accessToken,
+	    		method: 'GET',
+	    		dataType: 'json',
+	    		async: false
+
+	    	})
+
+	    	.done(function(response) {
+
+				pageID = response.id;
+
+			})
+
+			.fail(function(response) {
+
+				settings.postID = null;
+				console.log(response.status + ' - ' + response.responseJSON.error.message);
+
+			});
+
+			return pageID;
+
+	    }
+
+
+
+
+
+		/**
+		 * Grab an app access token from Facebook API
+		 * @param  {Int/String} clientID     The client ID provided by developers.facebook.com
+		 * @param  {Int/String} clientSecret The client secret provided by developers.facebook.com
+		 * @return {String}              	 The access token
+		 */
+		function getAccessToken(clientID, clientSecret) {
+
+			// Set app credentials
+			var credentials = {
+			    client_id: clientID,
+			    client_secret: clientSecret,
+			    grant_type: "client_credentials"
+			}
+
+			// Make request for access token using above credentials
+			$.ajax({
+			    url: "https://graph.facebook.com/oauth/access_token?" + $.param(credentials),
+			    type: 'GET',
+			    dataType: 'json',
+			    async: false
+			})
+
+			// If successful, set data for return
+			.done(function(response) {
+				accessToken = response.access_token;
+			})
+
+			// If failed, show the user an error
+			// Fail quietly in case stream is live
+			.fail(function(response) {
+				console.log(response.status + ' - ' + response.responseJSON.error.message);
+			});
+
+			return accessToken;
+
+		}
+
+
+
+
+
+		/**
+	     * Build the Facebook Graph query, sent the request and return the data
+	     * @return NULL
+	     */
+    	function getTotals() {
+
+    		// Build Facebook Graph query from reactions array
+			var query = reactions.map(function (e) {
+
+			    var code = e;
+			    var upper = e.toUpperCase();
+
+			    return 'reactions.type(' + upper + ').limit(0).summary(total_count).as(' + code + ')'
+
+			});
+
+			// Add total reactions to query
+			query.push('reactions.limit(0).summary(total_count).as(total)');
+
+
+
+			// Query Facebook Graph
+			$.ajax({
+				url: 'https://graph.facebook.com/v2.9/?ids=' + settings.postID + '&fields=' + query.join(',') + '&access_token=' + accessToken,
+				method: 'GET',
+				dataType: 'json'
+			})
+
+			// If successful, populate sections with updated totals
+			.done(function(response) {
+
+				var output = [];
+				var total = 0;
+
+
+				// Loop through the responses and get the total amount of votes for the active reactions
+				// This prevents using the totals from reactions that arent wanted and makes the percentages more accurate
+				$.each(response[settings.postID], function(index, value) {
+
+					if(index != 'total' && index != 'id') {
+						total = total + this.summary.total_count
+					}
+
+				});
+
+
+				// Loop through sections and update the totals
+				$.each(sections, function(index, value) {
+
+					var reaction = $(this).data('reaction');
+					var reactionTotal = response[settings.postID][reaction].summary.total_count;
+
+					$(this).find('.fbr-counter').text(settings.defaultCount + reactionTotal);
+					$(this).attr('data-reaction-total', reactionTotal).attr('data-reaction-percent', (reactionTotal / total) * 100);
+
+					// Push current total to console output string
+					output.push(reaction + " (" + reactionTotal +  " [" + ((reactionTotal / total) * 100).toFixed(1) + "%])");
+
+				});
+
+				// Set the totals
+				// Add the final total to the parent container
+				if(settings.totalType === 'selected') {
+					container.attr('data-reaction-total', total);
+					$(settings.totalSelector).text(total);
+				}else if(settings.totalType === 'all') {
+					container.attr('data-reaction-total', response[settings.postID].total.summary.total_count);
+					$(settings.totalSelector).text(response[settings.postID].total.summary.total_count);
+				}
+
+
+				// Show current totals for overview
+				if(settings.showOutput === true) {
+					output.push("total(" + total + ")");
+					console.log("Data: " + output.join(' | '));
+				}
+
+			})
+
+			// If failed, show the user an error
+			// Fail quietly in case stream is live
+			.fail(function(response) {
+				console.log(response.status + ' - ' + response.responseJSON.error.message);
+			});
+
+		}
+
+
+	}
+
+})(jQuery);
